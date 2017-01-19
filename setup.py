ac189c71624d94af85920e9bd5b912eb7ee678f3
astrojarred <astrojarred@gmail.com>
2017-01-19 23:54:14 +0100

    initialized setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='threebodyjg',
+      version='1.0',
+      description='A three-body simulation',
+      author='Jarred Green',
+      author_email='astrojarred@gmail.com',
+      url='https://github.com/astrojarred/threebodyjg',
+      scripts=['miracle']
+     )
